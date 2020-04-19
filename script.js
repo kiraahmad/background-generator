f7a7792acf2cad4285b4b6c228faa50e61295c15
kiraahmad <60839257+kiraahmad@users.noreply.github.com>
2020-04-19 21:45:07 +0800

    adding starting project

---
+++ script.js
@@ -0,0 +1,9 @@
+promt("how old are you?")
+if (Age === "+18") {
+	alert ("Power on. Enjoy the ride");
+} else if (Age === "18"); {
+	alert("Congratulations on your first year of driving. Enjoy the ride");
+} 
+else {
+	alert("Sorry, you are too young to drive this car. Powering off")
+}
